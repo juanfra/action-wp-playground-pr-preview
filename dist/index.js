66b405cab2fa69a5e635f68d7de81b126d3ec385
Juan Aldasoro <juanfraa@gmail.com>
2024-10-18 16:54:13 +0200

    Revert

--- dist/index.js
+++ dist/index.js
@@ -29231,7 +29231,7 @@ function createBlueprint(themeSlug, branch, repo, themeDir) {
     /* If themeDir is not provided, we assume that the action is running in a single theme workflow and the theme folder name will be the theme slug + the branch name.
      * If themeDir is provided, we assume that the action is running in a multi theme workflow and the theme folder name will be the theme slug.
      */
-    const themeFolderName = themeSlug;
+    const themeFolderName = !themeDir ? `${themeSlug}-${branch}` : themeSlug;
     (0, core_1.debug)(`Theme folder name: ${themeFolderName}`);
     const template = {
         steps: [
@@ -29255,6 +29255,9 @@ function createBlueprint(themeSlug, branch, repo, themeDir) {
                 themeData: {
                     resource: 'url',
                     url: buildProxyURL(repo, branch, themeDir),
+                },
+                options: {
+                    activate: true,
                 },
             },
         ],
